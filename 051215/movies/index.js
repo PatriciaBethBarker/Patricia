1ade30871d1348b321b4e1e49b36e60d0c4d99e8
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-05-19 16:52:05 -0700

    make adjustments to form

--- 051215/movies/index.js
+++ 051215/movies/index.js
@@ -1,12 +1,12 @@
 var hapi = require("hapi");
-var listings = require("./moviesData.json")
-
+//this is the strting point
 //load my movie listings by showing the path
-  console.log(listings)
+  //console.log(listings);
 var server = new hapi.Server();
 //constructors are always capitalized
 server.connection({ port: 8000});
 server.start();
+
 
 //set up our view system
 server.views({
@@ -18,61 +18,38 @@ server.views({
   //let hapi know about the layout template folder
   layoutPath: "layouts",
   layout: "default",
-  //add a partials path here
-  partialsPath: "templates/partials",
+  // //add a partials path here
+    partialsPath: "templates/partials",
   isCached: false
 });
 
+
+
+var jsonObj = require("./moviesData.json");
+jsonObj = jsonObj.movies;
+
+//localhost:8000/
 server.route({
   method: "GET",
   path: "/",
   handler: function(req, reply) {
     //we still just reply with a view
-    reply.view("inner", { title: "Text Page", movies: listings.movies})
+    reply.view("inner", {
+      title: "Text Page",
+      movies: jsonObj
+    })
   }
 });
 
 server.route({
   method: "GET",
   //we must use "param" here, not another route varibale name
-  path: "/assets/{param*}",
-  handler:  {
-    directory: {
-      path: "public"
-    }
-  }
-});
-
-
-
-var JsonMovies;
-fs.readFile() {
-
-
-}
-
-sever.route({
-  method: "GET",
-  path: "/",
-    handler: function(req, reply) {
-      fs.readFile("movies.json", "utf8", function(err, data) {
-        reply.view("movies", {
-          title: "Movies",
-          movies: JsonMovies
-
-          });
-      }
-});
-
-server.route({
-  method: "GET",
-  path: "/movie/{{index}}",
+  path: "/movies/{param*}",
   handler: function(req, reply) {
-    var movie = JsonMovies[req.params.index];
+    //we still just reply with a view
+    reply.view("movie", {
 
-    reply.view("movie", ){ //this is movie.html
-      title:  movie.title
 
-      });
-    }
+    })
+  }
 });
